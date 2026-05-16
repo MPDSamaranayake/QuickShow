dcb84d5db84a404b04cedad3ee765ca17985934e
prasad20001205 <prasadsamaranayake184@gmail.com>
2026-05-16 20:16:36 +0530

    feat: enhance MyBookings and SeatLayouts with booking display and seat selection functionality

--- client/src/pages/SeatLayout.jsx
+++ client/src/pages/SeatLayout.jsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { Clock3 } from "lucide-react";
+import { useNavigate, useParams } from "react-router-dom";
+import { ArrowUpRightIcon, Clock3 } from "lucide-react";
+import toast from "react-hot-toast";
 import { assets, dummyDateTimeData, dummyShowsData } from "../assets/assets";
 import isoTimeFormat from "../lib/isoTimeFormat";
 import BlurCircle from "../components/BlurCircle";
 
 const SeatLayouts = () => {
 
+  const groupRows = [["A", "B"], ["C", "D"], ["E", "F"], ["G", "H"], ["I", "J"]]
+
   const { id, date } = useParams();
+  const navigate = useNavigate();
   const [selectedSeats, setSelectedSeats] = useState([]);
   const [selectedTime, setSelectedTime] = useState(null);
   const [show, setShow] = useState(null);
@@ -20,7 +24,36 @@ const SeatLayouts = () => {
         dateTime: dummyDateTimeData,
       })
     }
-  };
+  }
+  
+  const handleSeatClick = (seatNumber) => {
+    if (!selectedTime) {
+      return toast("Please select a time first.");
+    }
+    if(!selectedSeats.includes(seatNumber) && selectedSeats.length > 4) {
+      return toast("You can only select 5 seats.");
+    }
+    setSelectedSeats(prev => prev.includes(seatNumber) ? prev.filter(seat => seat !== seatNumber) : [...prev, seatNumber])
+  }
+
+  const renderSeats = (row, count = 9) => (
+    <div key={row} className="flex gap-2 mt-2">
+      <div className="flex flex-wrap items-center justify-center gap-2">
+      {Array.from({ length: count }, (_, index) => {
+        const seatNumber = `${row}${index + 1}`;
+        return (
+          <button
+            key={seatNumber}
+            onClick={() => handleSeatClick(seatNumber)}
+            className={`w-7 h-7 rounded border border-primary/60 cursor-pointer flex items-center justify-center text-[10px] ${selectedSeats.includes(seatNumber) ? 'bg-primary text-white' : ''}`}
+          >
+            {seatNumber}
+          </button>
+        );
+      })}
+    </div>
+    </div>
+  )
 
   useEffect(() => {
     getShow();
@@ -59,6 +92,29 @@ const SeatLayouts = () => {
         <h1 className="text-2xl font-semibold mb-4">Select your seat</h1>
         <img src={assets.screenImage} alt="Screen" />
         <p className="text-gray-400 text-sm mb-6">SCREEN SIDE</p>
+
+        <div className="flex flex-col items-center mt-10 text-xs text-gray-300">
+          <div className="grid grid-cols-2 md:grid-cols-1 gap-8 md:gap-2 mb-6">
+            {groupRows[0].map(row => renderSeats(row))}
+          </div>
+
+        </div>
+        <div className="grid grid-cols-2 gap-11">
+          {groupRows.slice(1).map((group, index) => (
+            <div key={index}>
+              {group.map(row => renderSeats(row))}
+            </div>
+          ))}
+        </div>
+
+        <button
+          type="button"
+          onClick={() => navigate('/my-bookings')}
+          className="mt-10 flex items-center gap-1 px-6 py-2 text-sm bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer active:scale-95"
+        >
+          Proceed to Checkout
+          <ArrowUpRightIcon strokeWidth={3} className="w-4 h-4" />
+        </button>
 
       </div>
     </div>
